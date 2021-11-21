adf21375bb7728214c55afc2a2a8d1edeeed52ec
MohammedDev315 <mmohammaddev33@gmail.com>
2021-11-21 20:27:58 +0300

    get csv file

--- Practices/beautifulsoup/pr.py
+++ Practices/beautifulsoup/pr.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 with open("ht1.html") as ht1:
     soup = BeautifulSoup(ht1, "html5lib")
@@ -15,14 +16,38 @@ with open("ht1.html") as ht1:
 # print(len(all_divs))
 
 
+csv_file = open("result1.csv" , "w")
+write_to_csv = csv.writer(csv_file)
+write_to_csv.writerow(['image_link' , 'title' , 'text' , 'post_time' ])
+
 req1 = requests.get("https://news.mit.edu/topic/machine-learning").text
 soup2 = BeautifulSoup(req1, "html5lib")
 result1 = soup2.find_all("div" , class_="page-term--views--list-item" )
 for box in result1:
-    print(box.find("a", class_="term-page--news-article--item--title--link").span.text)
-    print(box.find("p", class_="term-page--news-article--item--dek").span.text)
-    print(box.find("time").text)
+    try:
+        img = box.find("img")["src"]
+    except:
+        img = None
+    try:
+        title = box.find("a", class_="term-page--news-article--item--title--link").span.text
+    except:
+        title = None
+    try:
+        text = box.find("p", class_="term-page--news-article--item--dek").span.text
+    except:
+        text = None
+    try:
+        time_r = box.find("time").text
+    except:
+        time_r = None
+    print(img)
+    print(title)
+    print(text)
+    print(time_r)
     print("========================")
+    write_to_csv.writerow([img , title , text , time_r])
+
+csv_file.close()
 
 
 
