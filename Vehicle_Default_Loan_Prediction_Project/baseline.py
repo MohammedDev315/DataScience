1dfc541fa27129a651051c124407273f089310c4
MohammedDev315 <mmohammaddev33@gmail.com>
2021-12-15 18:46:21 +0300

    Base line

--- Vehicle_Default_Loan_Prediction_Project/baseline.py
+++ Vehicle_Default_Loan_Prediction_Project/baseline.py
@@ -9,30 +9,40 @@ from sklearn.metrics import precision_score, recall_score, accuracy_score, roc_a
 from sklearn.metrics import confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
 
-
-df = pd.read_csv("data/train.csv")
+df = pd.read_csv("data/clean_data.csv")
+print(df.columns)
 #%%
 df_numeric = df.select_dtypes(include=np.number)
-x = df.drop("VOTERID_FLAG" , axis = 1)
-y = df["VOTERID_FLAG"]
-
-x_numeric = df_numeric.drop("VOTERID_FLAG" , axis = 1)
-y_numeric = df_numeric["VOTERID_FLAG"]
+x = df.drop("LOAN_DEFAULT" , axis = 1)
+y = df["LOAN_DEFAULT"]
+x_numeric = df_numeric.drop("LOAN_DEFAULT" , axis = 1)
+y_numeric = df_numeric["LOAN_DEFAULT"]
 
 X_train , X_vald , y_train , y_vald = train_test_split(x , y , test_size = 0.2, random_state=42)
-X_train_numeric , X_vald_numeric , y_train_numeric , y_vald_numeric = train_test_split(x_numeric , y_numeric , test_size = 0.2, random_state=42)
+
 
 #%%
 model = KNeighborsClassifier(n_neighbors=3)
-model.fit(X_vald_numeric , y_vald_numeric)
-print(f"Knn Train{model.score(X_train_numeric , y_train_numeric)}")
-print(f"Knn Val{model.score(X_vald_numeric , y_vald_numeric)}")
+model.fit(X_train , y_train)
+print(f"Knn Train{model.score(X_train , y_train)}")
+print(f"Knn Val{model.score(X_vald , y_vald)}")
+y_pred = model.predict(X_vald)
+cf_matrix = confusion_matrix(y_vald, y_pred)
+print(cf_matrix)
+sns.heatmap(cf_matrix, annot=True)
+plt.show()
 
 #%%
 model = LogisticRegression()
-model.fit(X_vald_numeric , y_vald_numeric)
-print(f"Logistic Train{model.score(X_train_numeric , y_train_numeric)}")
-print(f"Logistic Val{model.score(X_vald_numeric , y_vald_numeric)}")
+model.fit(X_train , y_train)
+print(f"Logistic Train{model.score(X_train , y_train)}")
+print(f"Logistic Val{model.score(X_vald , y_vald)}")
+y_pred = model.predict(X_vald)
+cf_matrix = confusion_matrix(y_vald, y_pred)
+print(cf_matrix)
+sns.heatmap(cf_matrix, annot=True)
+plt.show()
+
 
 
 
